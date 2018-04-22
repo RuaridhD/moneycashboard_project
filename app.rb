6a8a7c2dcec56e149119ae597cb46aa3518fef38
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-22 08:54:24 +0100

    adds controllers file structure

---
+++ app.rb
@@ -0,0 +1,5 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('controllers/merchants_controller')
+require_relative('controllers/transactions_controller')
+require_relative('controllers/types_controller')
